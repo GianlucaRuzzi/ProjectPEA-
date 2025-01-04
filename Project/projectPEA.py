9c2fdb8cfe5ee36dd1ea29cd927811716aec9875
GianlucaRuzzi <gianluca.ruzzi@mail.polimi.it>
2025-01-04 17:09:14 +0100

    Creation of folders and project file

---
+++ Project/projectPEA.py
@@ -0,0 +1,39 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import deque
+from scipy import linalg
+
+
+# Station1=Terminal
+# Station2= CPU
+# Station3= Transfer
+# Station4= COP AI
+S1=0 #Think time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sys.exit()
